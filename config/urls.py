3c27e4253dd8dad8474e4c71456caaaea62b2547
Pavlo Semenikhin <pashock1337@gmail.com>
2025-11-12 10:47:35 +0200

    Add swagger and include urls.py of accounts package

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/user/", include("accounts.urls", namespace="accounts")),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ]
