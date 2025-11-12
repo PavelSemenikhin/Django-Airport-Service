993e7b71106769fe5451f01cf36dab579850f97c
Pavlo Semenikhin <pashock1337@gmail.com>
2025-11-12 10:49:24 +0200

    Add views for creating users and managing profiles

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import permissions, viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from accounts.serializers import (
+    RegisterSerializer,
+    ProfileSerializer,
+)
+
+User = get_user_model()
+
+
+class CreateUserViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = (permissions.AllowAny,)
+
+
+class ProfileViewSet(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = User.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (JWTAuthentication,)
+    http_method_names = ["get", "put", "patch"]
+
+    def get_object(self):
+        return self.request.user.profile  # noqa
