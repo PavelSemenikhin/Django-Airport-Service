4fb88555a65edab434576c87b71723a85f4b209b
Pavlo Semenikhin <pashock1337@gmail.com>
2025-11-17 18:50:29 +0200

    Add docstring

--- airport_service/custom_mixins.py
+++ airport_service/custom_mixins.py
@@ -3,6 +3,8 @@ from rest_framework.permissions import IsAdminUser, AllowAny
 
 
 class AdminOrReadOnly(viewsets.ModelViewSet):
+    """Allow read-only access for unauthenticated users, full access for admins."""
+
     def get_permissions(self):
         if self.action in ["create", "update", "partial_update", "destroy"]:
             return [IsAdminUser()]
